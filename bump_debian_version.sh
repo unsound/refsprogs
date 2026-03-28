42ec31faee89d2256296e7755acc97ed9ad0be00
Erik Larsson <catacombae@gmail.com>
2026-03-28 06:02:10 +0100

    Update Debian minor version.
    Also update bump_debian_version.sh to support updating the minor
    version.

--- bump_debian_version.sh
+++ bump_debian_version.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-    echo "usage: bump_debian_version.sh <version> <library version>"
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+    echo "usage: bump_debian_version.sh <version> <library version> [<revision>]"
     exit 1
 fi
 
 VERSION="$1"
 LIBRARY_VERSION="$2"
+if [ $# -eq 3 ]; then
+    REVISION="$3"
+else
+    REVISION="1"
+fi
 
 set -x
 
-# Change the library version of librefs*.install.
-git mv debian/librefs[0-9]*.install debian/librefs${LIBRARY_VERSION}.install || exit 1
+if [ ! -f debian/librefs${LIBRARY_VERSION}.install ]; then
+    # Change the library version of librefs*.install.
+    git mv debian/librefs[0-9]*.install debian/librefs${LIBRARY_VERSION}.install || exit 1
+fi
 
 # Update library version in debian/control.
 ( sed "s/librefs[0-9][0-9]*/librefs${LIBRARY_VERSION}/g" debian/control > debian/control.new && mv debian/control.new debian/control ) || exit 1
@@ -20,7 +27,7 @@ git mv debian/librefs[0-9]*.install debian/librefs${LIBRARY_VERSION}.install || 
 ( sed "s/librefs[0-9][0-9]*/librefs${LIBRARY_VERSION}/g" debian/refsprogs-udeb.lintian-overrides > debian/refsprogs-udeb.lintian-overrides.new && mv debian/refsprogs-udeb.lintian-overrides.new debian/refsprogs-udeb.lintian-overrides ) || exit 1
 
 # Generate changelog entry.
-echo "refsprogs (${VERSION}-1) main; urgency=medium" > debian/changelog.new || exit 1
+echo "refsprogs (${VERSION}-${REVISION}) main; urgency=medium" > debian/changelog.new || exit 1
 echo "" >> debian/changelog.new || exit 1
 echo "  * New upstream release." >> debian/changelog.new || exit 1
 echo "" >> debian/changelog.new || exit 1
@@ -31,7 +38,7 @@ mv -v debian/changelog.new debian/changelog || exit 1
 
 # Generate local changelog entry.
 
-echo "Version ${VERSION}-1 ($(LANG=C date '+%b %e'))" > debian/local/changelog.new || exit 1
+echo "Version ${VERSION}-${REVISION} ($(LANG=C date '+%b %e'))" > debian/local/changelog.new || exit 1
 echo "" >> debian/local/changelog.new || exit 1
 echo "     * New upstream release." >> debian/local/changelog.new
 echo "" >> debian/local/changelog.new || exit 1
