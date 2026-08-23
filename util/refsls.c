67ca03bbf8ba3297bc65481abfd88f462c867182
Erik Larsson <catacombae@gmail.com>
2026-08-23 09:40:27 +0300

    refsls.c: Use the node cache when listing directories.

--- util/refsls.c
+++ util/refsls.c
@@ -317,7 +317,7 @@ static int refsls_print_dirent(
 			/* refs_block_map **block_map */
 			&subdir_ctx.vol->block_map,
 			/* refs_node_cache **node_cache */
-			NULL,
+			&subdir_ctx.vol->node_cache,
 			/* const u64 *start_node */
 			NULL,
 			/* const u64 *object_id */
@@ -725,7 +725,7 @@ int main(int argc, char **argv)
 		/* refs_block_map **block_map */
 		&vol->block_map,
 		/* refs_node_cache **node_cache */
-		NULL,
+		&vol->node_cache,
 		/* const u64 *start_node */
 		NULL,
 		/* const u64 *object_id */
